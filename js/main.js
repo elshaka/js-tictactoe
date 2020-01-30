a3ad6d68ec9e7441f166147f5a2049377bee8c2f
Aaron-RN <aaron.rory.newbold@gmail.com>
2020-01-30 14:04:29 -0500

    Added isFinished
    Added isFinished Method

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ const Player = name => ({ name });
 
 const game = ((player1, player2) => {
   let currentPlayer = player1;
-  const finished = false;
-  const winner = null;
+  let finished = false;
+  let winner = null;
   const board = [
     [null, null, null],
     [null, null, null],
@@ -11,8 +11,28 @@ const game = ((player1, player2) => {
   ];
 
   const getBoard = () => board;
-  const isFinished = () => {};
+  const isFinished = () => {
+    console.log(board.flat());
+    const flatBoard = board.flat();
+    const winning_combo = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+    
+    const gameWon = winning_combo.some( (i) => {
+      if (flatBoard[i[0]] === flatBoard[i[1]] && flatBoard[i[1]] === flatBoard[i[2]] && flatBoard[i[0]] != null)
+      {
+        console.log(currentPlayer.name + "has won!");
+        finished = true;
+        winner = currentPlayer;
+        return true;
+      }
+    });
+        
+    const boardisFull = board.flat().every( (x) => x !== null )
+    if(boardisFull) {console.log("Full"); finished = true;}
+    
+    return finished;
+  };
   const play = (move) => {
+    if (finished) {return false;}
     if (move < 1 || move > 9) {
       return false;
     }
